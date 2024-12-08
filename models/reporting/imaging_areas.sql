45c6dd7dc5f9cac997d8b2153ac0fb012cb7274b
julianam-w <77252335+julianam-w@users.noreply.github.com>
2024-12-08 19:37:47 +1100

    Update

--- models/reporting/imaging_areas.sql
+++
@@ -1,6 +0,0 @@
-select
-    id,
-    imaging_request_id,
-    area_id
-from {{ source("tamanu", "imaging_request_areas") }}
-where deleted_at is null
---
+++ models/reporting/imaging_request_areas.sql
@@ -0,0 +1,11 @@
+select
+    ira.id,
+    ira.imaging_request_id,
+    ira.area_id
+from {{ source("tamanu", "imaging_request_areas") }} ira
+join {{ source("tamanu", "imaging_requests") }} ir on ir.id = ira.imaging_request_id
+join {{ source("tamanu", "encounters") }} e on e.id = ir.encounter_id
+where ira.deleted_at is null
+    and ir.deleted_at is null
+    and e.deleted_at is null
+    and e.patient_id != '{{ var("test_patient") }}'
