1fd55cf5ecfd580adc604dd9920022af2710aa88
julianam-w <77252335+julianam-w@users.noreply.github.com>
2024-12-08 21:57:56 +1100

    Update patient_death_contributing_causes.sql

--- models/reporting/patient_death_contributing_causes.sql
+++ models/reporting/patient_death_contributing_causes.sql
@@ -7,4 +7,4 @@ FROM {{ source("tamanu", "contributing_death_causes") }} cdc
 join {{ source("tamanu", "patient_death_data") }} pdd on pdd.id = cdc.patient_death_data_id
 where cdc.deleted_at is null
     and pdd.deleted_at is null
-    and pdd.patient_id != {{ var("test_patient") }}
+    and pdd.patient_id != '{{ var("test_patient") }}'
