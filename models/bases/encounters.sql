270038e95c0bf063866615442aa44430db54e4a8
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-02-10 17:25:40 +1100

    revert

--- models/bases/encounters.sql
+++ models/bases/encounters.sql
@@ -1,15 +1,13 @@
 select
     id,
     start_date::timestamp as start_datetime,
-    case when end_date < start_date then start_date::timestamp
-        else end_date::timestamp
-    end as end_datetime,
+    case when end_date < start_date then start_date::timestamp else end_date::timestamp end as end_datetime,
     encounter_type,
     reason_for_encounter,
     device_id,
     patient_id,
     department_id
-    as location_id,
+    location_id,
     examiner_id as clinician_id,
     patient_billing_type_id,
     referral_source_id,
