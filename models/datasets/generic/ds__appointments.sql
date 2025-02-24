b621fdd4e83b62dd9c3665c471c186e5bb69448e
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-02-24 11:59:28 +1100

    appointment line list report

---
+++ models/datasets/generic/ds__appointments.sql
@@ -0,0 +1,33 @@
+select
+    p.id as patient_id,
+    p.display_id,
+    p.first_name,
+    p.last_name,
+    p.date_of_birth,
+    date_part('year', age(a.start_time::date, p.date_of_birth::date)) as age,
+    initcap(p.sex::text) as sex,
+    vil.id as village_id,
+    vil.name as village,
+    billing.id as billing_type_id,
+    billing.name as billing_type,
+    a.start_time as appointment_start_time,
+    a.end_time as appointment_end_time,
+    apt.appointment_type_id,
+    apt.name as appointment_type,
+    a.status as appointment_status,
+    u.id as clinician_id,
+    u.display_name as clinician,
+    lg.id as location_group_id,
+    lg.name as location_group,
+    bt.id as booking_type_id,
+    bt.name as booking_type
+from {{ ref('appointments') }} a
+join {{ ref('patients') }} p on p.id = a.patient_id
+left join {{ ref('users') }} u on u.id = a.clinician_id
+left join {{ ref('location_groups') }} lg on lg.id = a.location_group_id
+left join {{ ref('patient_additional_data') }} pd on pd.patient_id = p.id
+left join {{ ref('reference_data') }} billing on billing.id = pd.patient_billing_type_id
+left join {{ ref('reference_data') }} vil on vil.id = p.village_id
+left join {{ ref('reference_data') }} apt on apt.id = a.appointment_type_id
+left join {{ ref('reference_data') }} bt on bt.id = a.booking_type_id
+order by a.start_time
---
+++ models/reports/sql/generic/upcoming-appointments-line-list.sql
@@ -0,0 +1,39 @@
+select
+    display_id as "{{ translate_string('general.localisedField.displayId.label', 'Patient ID') }}",
+    first_name as "{{ translate_string('general.localisedField.firstName.label', 'First name') }}",
+    last_name as "{{ translate_string('general.localisedField.lastName.label', 'Last name') }}",
+    to_char(date_of_birth, '{{ var("date_format") }}') as "{{ translate_string('general.localisedField.dateOfBirth.label', 'Date of birth') }}",
+    age as "{{ translate_string('general.localisedField.Age', 'Age') }}",
+    sex as "{{ translate_string('general.localisedField.sex.label', 'Sex') }}",
+    village as "{{ translate_string('general.localisedField.villageId.label','Village') }}",
+    billing_type as "{{ translate_string('general.localisedField.patientBillingTypeId.label', 'Patient type') }}",
+    to_char(appointment_start_time, '{{ var("datetime_format") }}') as "Appointment date and time",
+    appointment_type as "{{ translate_string('scheduling.newAppointment.type.label', 'Appointment type') }}",
+    appointment_status as "{{ translate_string('', 'Appointment status') }}",
+    clinician as "{{ translate_string('general.localisedField.clinician.label', 'Clinician') }}",
+    location_group as "{{ translate_string('general.localisedField.area.label', 'Area') }}"
+from {{ ref('ds__appointments') }}
+where case
+        when{{ parameter('fromDate', default_value='2024-01-01', data_type='date') }} is null then true
+        else appointment_start_time >={{ parameter('fromDate', default_value='2024-01-01', data_type='date') }}
+    end
+    and
+    case
+        when{{ parameter('toDate', default_value='2024-01-31', data_type='date') }} is null then true
+        else appointment_start_time <={{ parameter('toDate', default_value='2024-01-31', data_type='date') }}
+    end
+    and
+    case
+        when{{ parameter('locationGroupId') }} is null then true
+        else location_group_id ={{ parameter('locationGroupId') }}
+    end
+    and
+    case
+        when{{ parameter('appointmentStatus') }} is null then true
+        else appointment_status ={{ parameter('appointmentStatus') }}
+    end
+    and
+    case
+        when{{ parameter('clinicianId') }} is null then true
+        else clinician_id ={{ parameter('clinicianId') }}
+    end
