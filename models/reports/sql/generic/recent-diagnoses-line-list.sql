39f63dfe7db07999aaa321c89ffe0841ccebefb9
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-04-15 20:06:23 +1000

    Update recent-diagnoses-line-list.sql

--- models/reports/sql/generic/recent-diagnoses-line-list.sql
+++ models/reports/sql/generic/recent-diagnoses-line-list.sql
@@ -1,17 +1,17 @@
 select
-    to_char(diagnosis_datetime, '{{ var("datetime_format") }}') as "{{ translate_string('', 'Date & time') }}",
-    diagnosis as "{{ translate_string('general.diagnosis.label', 'Diagnosis') }}",
-    first_name as "{{ translate_string('general.localisedField.firstName.label', 'First name') }}",
-    last_name as "{{ translate_string('general.localisedField.lastName.label', 'Last name') }}",
-    display_id as "{{ translate_string('general.localisedField.displayId.label', 'Patient ID') }}",
-    age as "{{ translate_string('general.localisedField.Age', 'Age') }}",
-    sex as "{{ translate_string('general.localisedField.sex.label', 'Sex') }}",
-    contact_number as "{{ translate_string('general.contactNumber.label', 'Contact number') }}",
-    village as "{{ translate_string('general.localisedField.villageId.label', 'Village') }}",
-    clinician as "{{ translate_string('general.localisedField.clinician.label', 'Clinician') }}",
-    department as "{{ translate_string('general.localisedField.departmentId.label', 'Department') }}",
-    certainty as "{{ translate_string('diagnosis.certainty.label', 'Certainty') }}",
-    is_primary as "{{ translate_string('diagnosis.isPrimary.label', 'Is primary') }}"
+    to_char(diagnosis_datetime, '{{ var("datetime_format") }}') as "{{ translate_string('diagnosisDateTime', 'Date & time') }}",
+    diagnosis as "{{ translate_string('diagnosisName', 'Diagnosis') }}",
+    first_name as "{{ translate_string('patientFirstName', 'First name') }}",
+    last_name as "{{ translate_string('patientLastName', 'Last name') }}",
+    display_id as "{{ translate_string('patientDisplayId', 'Patient ID') }}",
+    age as "{{ translate_string('patientAge', 'Age') }}",
+    sex as "{{ translate_string('patientSex', 'Sex') }}",
+    contact_number as "{{ translate_string('patientPrimaryContactNumber', 'Contact number') }}",
+    village as "{{ translate_string('patientVillage', 'Village') }}",
+    clinician as "{{ translate_string('encounterClinician', 'Clinician') }}",
+    department as "{{ translate_string('departmentName', 'Department') }}",
+    certainty as "{{ translate_string('diagnosisCertainty', 'Certainty') }}",
+    is_primary as "{{ translate_string('diagnosisIsPrimary', 'Is primary') }}"
 from {{ ref('ds__diagnoses') }}
 where
     case
@@ -28,21 +28,27 @@ where
     and
     case
         when {{ parameter('facilityId') }} is null then true
-        else facility_id ={{ parameter('facilityId') }}
+        else facility_id = {{ parameter('facilityId') }}
     end
     and
     case
         when {{ parameter('villageId') }} is null then true
-        else village_id ={{ parameter('villageId') }}
+        else village_id = {{ parameter('villageId') }}
     end
     and
     case
         when {{ parameter('clinicianId') }} is null then true
-        else clinician_id ={{ parameter('clinicianId') }}
+        else clinician_id = {{ parameter('clinicianId') }}
     end
     and
     case when
             coalesce({{ parameter('diagnosisId') }}, {{ parameter('diagnosis2Id') }}, {{ parameter('diagnosis3Id') }}, {{ parameter('diagnosis4Id') }}, {{ parameter('diagnosis5Id') }}) is null
             then true
-        else diagnosis_id in ({{ parameter('diagnosisId') }}, {{ parameter('diagnosis2Id') }}, {{ parameter('diagnosis3Id') }}, {{ parameter('diagnosis4Id') }}, {{ parameter('diagnosis5Id') }})
+        else diagnosis_id in (
+            {{ parameter('diagnosisId') }},
+            {{ parameter('diagnosis2Id') }},
+            {{ parameter('diagnosis3Id') }},
+            {{ parameter('diagnosis4Id') }},
+            {{ parameter('diagnosis5Id') }}
+        )
     end
