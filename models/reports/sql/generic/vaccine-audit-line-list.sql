a27f06e47e407edc3af93c849747d05ae3463d1e
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-04-15 17:29:39 +1000

    Update vaccine-audit-line-list.sql

--- models/reports/sql/generic/vaccine-audit-line-list.sql
+++ models/reports/sql/generic/vaccine-audit-line-list.sql
@@ -1,21 +1,21 @@
 select
-    display_id as "{{ translate_string('general.localisedField.displayId.label', 'Patient ID') }}",
-    first_name as "{{ translate_string('general.localisedField.firstName.label', 'First name') }}",
-    last_name as "{{ translate_string('general.localisedField.lastName.label', 'Last name') }}",
-    to_char(date_of_birth, '{{ var("date_format") }}') as "{{ translate_string('general.localisedField.dateOfBirth.label', 'Date of birth') }}",
-    age as "{{ translate_string('general.localisedField.Age', 'Age') }}",
-    sex as "{{ translate_string('general.localisedField.sex.label', 'Sex') }}",
-    village as "{{ translate_string('general.localisedField.villageId.label', 'Village') }}",
-    to_char(vaccination_date, '{{ var("date_format") }}') as "{{ translate_string('vaccine.dateRecorded.label', 'Vaccination date') }}",
-    vaccine_name as "{{ translate_string('vaccine.vaccineName.label', 'Vaccine name') }}",
-    vaccine_brand as "{{ translate_string('', 'If category of Other, Vaccine brand') }}",
-    disease as "{{ translate_string('', 'If category of Other, Disease') }}",
-    vaccine_status as "{{ translate_string('general.localisedField.vaccinationStatus.label', 'Vaccine status') }}",
-    vaccine_schedule as "{{ translate_string('vaccine.schedule.label', 'Schedule') }}",
-    given_by as "{{ translate_string('vaccine.givenBy.label', 'Given by') }}",
-    recorded_by as "{{ translate_string('vaccine.recordedBy.label', 'Recorded by') }}",
-    modified_by as "{{ translate_string('', 'Record modified by') }}",
-    to_char(modification_datetime, '{{ var("date_format") }}') as "{{ translate_string('', 'Record modification date') }}"
+    display_id as "{{ translate_string('patientDisplayId', 'Patient ID') }}",
+    first_name as "{{ translate_string('patientFirstName', 'First name') }}",
+    last_name as "{{ translate_string('patientLastName', 'Last name') }}",
+    to_char(date_of_birth, '{{ var("date_format") }}') as "{{ translate_string('patientDob', 'Date of birth') }}",
+    age as "{{ translate_string('patientAge', 'Age') }}",
+    sex as "{{ translate_string('patientSex', 'Sex') }}",
+    village as "{{ translate_string('patientVillage', 'Village') }}",
+    to_char(vaccination_date, '{{ var("date_format") }}') as "{{ translate_string('vaccinationDate', 'Vaccination date') }}",
+    vaccine_name as "{{ translate_string('vaccineName', 'Vaccine name') }}",
+    vaccine_brand as "{{ translate_string('vaccineBrand', 'If category of Other, Vaccine brand') }}",
+    disease as "{{ translate_string('vaccineDisease', 'If category of Other, Disease') }}",
+    vaccine_status as "{{ translate_string('vaccineStatus', 'Vaccine status') }}",
+    vaccine_schedule as "{{ translate_string('vaccineSchedule', 'Schedule') }}",
+    given_by as "{{ translate_string('givenBy', 'Given by') }}",
+    recorded_by as "{{ translate_string('recordedBy', 'Recorded by') }}",
+    modified_by as "{{ translate_string('modifiedBy', 'Record modified by') }}",
+    to_char(modification_datetime, '{{ var("date_format") }}') as "{{ translate_string('modifiedDate', 'Record modification date') }}"
 from {{ ref("ds__vaccinations") }}
 where
     vaccine_status in ('Recorded in error', 'Historical')
@@ -23,36 +23,36 @@ where
     case
         when {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }} is null then true
         else vaccination_date::date
-            >={{ parameter('fromDate', default_value='2024-01-01', data_type='date') }}
+            >= {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }}
     end
     and
     case
         when {{ parameter('toDate', default_value='2024-01-31', data_type='date') }} is null then true
         else vaccination_date::date
-            <={{ parameter('toDate', default_value='2024-01-31', data_type='date') }}
+            <= {{ parameter('toDate', default_value='2024-01-31', data_type='date') }}
     end
     and
     case
         when {{ parameter('villageId') }} is null then true
-        else village_id ={{ parameter('villageId') }}
+        else village_id = {{ parameter('villageId') }}
     end
     and
     case
         when {{ parameter('facilityId') }} is null then true
-        else facility_id ={{ parameter('facilityId') }}
+        else facility_id = {{ parameter('facilityId') }}
     end
     and
     case
         when {{ parameter('category') }} is null then true
-        else vaccine_category ={{ parameter('category') }}
+        else vaccine_category = {{ parameter('category') }}
     end
     and
     case
         when {{ parameter('vaccine') }} is null then true
-        else vaccine_name ={{ parameter('vaccine') }}
+        else vaccine_name = {{ parameter('vaccine') }}
     end
     and
     case
         when {{ parameter('status') }} is null then true
-        else vaccine_status ={{ parameter('status') }}
+        else vaccine_status = {{ parameter('status') }}
     end
