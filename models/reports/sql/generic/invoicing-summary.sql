ae759276bbd8d934f54513bf796159558cbe7dd2
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-04-15 19:35:00 +1000

    Update invoicing-summary.sql

--- models/reports/sql/generic/invoicing-summary.sql
+++ models/reports/sql/generic/invoicing-summary.sql
@@ -1,18 +1,18 @@
 select
-    to_char(i.admission_datetime, '{{ var("date_format") }}') as "{{ translate_string('discharge.admissionDate.label','Admission date') }}",
-    to_char(i.discharge_datetime, '{{ var("date_format") }}') as "{{ translate_string('discharge.dischargeDate.label','Discharged date') }}",
-    i.invoice_number as "{{ translate_string('general.localisedField.invoiceDisplayId.label','Invoice number') }}",
-    i.patient_name as "{{ translate_string('general.table.column.patientName','Patient name') }}",
-    i.display_id as "{{ translate_string('general.localisedField.displayId.label','Patient ID') }}",
-    i.social_security_number as "{{ translate_string('refData.patientFieldDefinition.fieldCategory-SocialSecurityNumber','Social security number') }}",
-    i.nationality as "{{ translate_string('general.localisedField.nationalityId.label','Nationality') }}",
-    i.insurers as "{{ translate_string('','Insurer') }}",
-    i.total_invoice_amount as "{{ translate_string('','Total invoice amount') }}",
-    i.total_insurer_amount as "{{ translate_string('','Total insurer amount') }}",
-    i.total_patient_discount as "{{ translate_string('','Total patient discount') }}",
-    i.total_patient_amount as "{{ translate_string('','Total patient amount') }}",
-    i.is_deceased as "{{ translate_string('','Deceased/Active') }}",
-    to_char(i.date_of_death, '{{ var("date_format") }}') as "{{ translate_string('','Date deceased') }}"
+    to_char(i.admission_datetime, '{{ var("date_format") }}') as "{{ translate_string('admissionDate','Admission date') }}",
+    to_char(i.discharge_datetime, '{{ var("date_format") }}') as "{{ translate_string('dischargeDate','Discharged date') }}",
+    i.invoice_number as "{{ translate_string('invoiceNumber','Invoice number') }}",
+    i.patient_name as "{{ translate_string('patientFullName','Patient name') }}",
+    i.display_id as "{{ translate_string('patientDisplayId','Patient ID') }}",
+    i.social_security_number as "{{ translate_string('patientSSN','Social security number') }}",
+    i.nationality as "{{ translate_string('patientNationality','Nationality') }}",
+    i.insurers as "{{ translate_string('invoiceInsurers','Insurer') }}",
+    i.total_invoice_amount as "{{ translate_string('invoiceTotalAmount','Total invoice amount') }}",
+    i.total_insurer_amount as "{{ translate_string('invoiceInsurerAmount','Total insurer amount') }}",
+    i.total_patient_discount as "{{ translate_string('invoicePatientDiscount','Total patient discount') }}",
+    i.total_patient_amount as "{{ translate_string('invoicePatientAmount','Total patient amount') }}",
+    i.is_deceased as "{{ translate_string('patientIsDeceased','Deceased/Active') }}",
+    to_char(i.date_of_death, '{{ var("date_format") }}') as "{{ translate_string('patientDateOfDeath','Date deceased') }}"
 from {{ ref("ds__invoicing") }} i
 where i.status = 'finalised'
     and case
