532b2bb064c229db86c1d8a8a08ec4f7e8ac2f16
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-04-15 18:01:07 +1000

    translation

--- models/reports/sql/generic/imaging-requests-line-list.sql
+++ models/reports/sql/generic/imaging-requests-line-list.sql
@@ -1,24 +1,24 @@
 select
-    display_id as "{{ translate_string('general.localisedField.displayId.label', 'Patient ID') }}",
-    first_name as "{{ translate_string('general.localisedField.firstName.label', 'First name') }}",
-    last_name as "{{ translate_string('general.localisedField.lastName.label', 'Last name') }}",
-    to_char(date_of_birth, '{{ var("date_format") }}') as "{{ translate_string('general.localisedField.dateOfBirth.label', 'Date of birth') }}",
-    age as "{{ translate_string('general.localisedField.Age', 'Age') }}",
-    sex as "{{ translate_string('general.localisedField.sex.label', 'Sex') }}",
-    village as "{{ translate_string('general.localisedField.villageId.label', 'Village') }}",
-    facility as "{{ translate_string('general.localisedField.facility.label', 'Facility') }}",
-    department as "{{ translate_string('general.localisedField.departmentId.label', 'Department') }}",
-    location_group as "{{ translate_string('general.localisedField.area.label', 'Area') }}",
-    request_id as "{{ translate_string('general.localisedField.requestId.label', 'Request ID') }}",
-    to_char(requested_datetime, '{{ var("datetime_format") }}') as "{{ translate_string('imaging.requestedDate.label', 'Request date and time') }}",
-    supervising_clinician as "{{ translate_string('general.supervisingClinician.label', 'Supervising clinician') }}",
-    requesting_clinician as "{{ translate_string('general.requestingClinician.label', 'Requesting clinician') }}",
-    priority as "{{ translate_string('general.localisedField.priority.label', 'Priority') }}",
-    imaging_type as "{{ translate_string('imaging.imagingType.label', 'Imaging type') }}",
-    imaging_area as "{{ translate_string('', 'Area to be imaged') }}",
-    status as "{{ translate_string('general.localisedField.status.label', 'Status') }}",
-    to_char(completed_datetime, '{{ var("datetime_format") }}') as "{{ translate_string('imaging.completedDate.label', 'Completed date and time') }}",
-    reason_for_cancellation as "{{ translate_string('imaging.modal.cancel.reason.label', 'Reason for cancellation') }}"
+    display_id as "{{ translate_string('patientDisplayId', 'Patient ID') }}",
+    first_name as "{{ translate_string('patientFirstName', 'First name') }}",
+    last_name as "{{ translate_string('patientLastName', 'Last name') }}",
+    to_char(date_of_birth, '{{ var("date_format") }}') as "{{ translate_string('patientDob', 'Date of birth') }}",
+    age as "{{ translate_string('patientAge', 'Age') }}",
+    sex as "{{ translate_string('patientSex', 'Sex') }}",
+    village as "{{ translate_string('patientVillage', 'Village') }}",
+    facility as "{{ translate_string('patientFacility', 'Facility') }}",
+    department as "{{ translate_string('patientDepartment', 'Department') }}",
+    location_group as "{{ translate_string('patientArea', 'Area') }}",
+    request_id as "{{ translate_string('imagingRequestId', 'Request ID') }}",
+    to_char(requested_datetime, '{{ var("datetime_format") }}') as "{{ translate_string('imagingRequestedDate', 'Request date and time') }}",
+    supervising_clinician as "{{ translate_string('supervisingClinician', 'Supervising clinician') }}",
+    requesting_clinician as "{{ translate_string('requestingClinician', 'Requesting clinician') }}",
+    priority as "{{ translate_string('imagingPriority', 'Priority') }}",
+    imaging_type as "{{ translate_string('imagingType', 'Imaging type') }}",
+    imaging_area as "{{ translate_string('imagingArea', 'Area to be imaged') }}",
+    status as "{{ translate_string('imagingStatus', 'Status') }}",
+    to_char(completed_datetime, '{{ var("datetime_format") }}') as "{{ translate_string('imagingCompletedDate', 'Completed date and time') }}",
+    reason_for_cancellation as "{{ translate_string('imagingCancellationReason', 'Reason for cancellation') }}"
 from {{ ref('ds__imaging_requests') }}
 where case
         when {{ parameter('fromDate', default_value='2024-01-01', data_type='timestamp') }} is null then true
@@ -34,20 +34,20 @@ where case
     and
     case
         when {{ parameter('requestedById') }} is null then true
-        else requesting_clinician_id ={{ parameter('requestedById') }}
+        else requesting_clinician_id = {{ parameter('requestedById') }}
     end
     and
     case
         when {{ parameter('imagingType') }} is null then true
-        else imaging_type_id ={{ parameter('imagingType') }}
+        else imaging_type_id = {{ parameter('imagingType') }}
     end
     and
     case
         when {{ parameter('statusId') }} is null then true
-        else status_id ={{ parameter('statusId') }}
+        else status_id = {{ parameter('statusId') }}
     end
     and
     case
         when {{ parameter('facilityId') }} is null then true
-        else facility_id ={{ parameter('facilityId') }}
+        else facility_id = {{ parameter('facilityId') }}
     end
