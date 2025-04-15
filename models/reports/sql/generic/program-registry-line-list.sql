0e189b0e641c0dad0d4c71db20a9c045db8c4d8a
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-04-15 20:01:11 +1000

    Update program-registry-line-list.sql

--- models/reports/sql/generic/program-registry-line-list.sql
+++ models/reports/sql/generic/program-registry-line-list.sql
@@ -1,16 +1,16 @@
 select
-    display_id as "{{ translate_string('general.localisedField.displayId.label','Patient ID') }}",
-    first_name as "{{ translate_string('general.localisedField.firstName.label','First name') }}",
-    last_name as "{{ translate_string('general.localisedField.lastName.label','Last name') }}",
-    to_char(date_of_birth, '{{ var("date_format") }}') as "{{ translate_string('general.localisedField.dateOfBirth.label', 'Date of birth') }}",
-    sex as "{{ translate_string('general.localisedField.sex.label', 'Sex') }}",
-    village as "{{ translate_string('general.localisedField.villageId.label', 'Village') }}",
-    registering_facility as "{{ translate_string('general.localisedField.facility.label', 'Registering facility') }}",
-    registered_by as "{{ translate_string('', 'Registered by') }}",
-    currently_at as "{{ translate_string('', 'Currently in') }}",
-    related_conditions as "{{ translate_string('', 'Related conditions') }}",
-    clinical_status as "{{ translate_string('', 'Status') }}",
-    to_char(registration_datetime, '{{ var("date_format") }}') as "{{ translate_string('', 'Date of Registration') }}"
+    display_id as "{{ translate_string('patientDisplayId', 'Patient ID') }}",
+    first_name as "{{ translate_string('patientFirstName', 'First name') }}",
+    last_name as "{{ translate_string('patientLastName', 'Last name') }}",
+    to_char(date_of_birth, '{{ var("date_format") }}') as "{{ translate_string('patientDateOfBirth', 'Date of birth') }}",
+    sex as "{{ translate_string('patientSex', 'Sex') }}",
+    village as "{{ translate_string('patientVillage', 'Village') }}",
+    registering_facility as "{{ translate_string('registryRegisteringFacility', 'Registering facility') }}",
+    registered_by as "{{ translate_string('registryRegisteredBy', 'Registered by') }}",
+    currently_at as "{{ translate_string('registryCurrentIn', 'Currently in') }}",
+    related_conditions as "{{ translate_string('registryConditions', 'Related conditions') }}",
+    clinical_status as "{{ translate_string('registryClinicalStatus', 'Status') }}",
+    to_char(registration_datetime, '{{ var("date_format") }}') as "{{ translate_string('registryRegisteredDate', 'Date of Registration') }}"
 from {{ ref('ds__patient_program_registrations') }}
 where registration_status = 'active'
     and is_most_recent
