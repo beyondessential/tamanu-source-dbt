1dc16ff15e07544c622d58d4866a75ac1c41cdec
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-04-15 17:43:40 +1000

    translation

--- models/reports/sql/generic/user-audit-report.sql
+++ models/reports/sql/generic/user-audit-report.sql
@@ -1,27 +1,28 @@
 select
-    user_name as "{{ translate_string('', 'User name') }}",
-    user_role as "{{ translate_string('', 'User role') }}",
-    display_id as "{{ translate_string('general.localisedField.displayId.label','Patient ID') }}",
-    patient_category as "{{ translate_string('general.localisedField.patientBillingTypeId.label', 'Patient category') }}",
-    triage_category as "{{ translate_string('', 'Triage category') }}",
-    facility as "{{ translate_string('general.localisedField.facility.label', 'Facility') }}",
-    department as "{{ translate_string('general.localisedField.departmentId.label', 'Department') }}",
-    location_group as "{{ translate_string('general.localisedField.area.label', 'Area') }}",
-    location as "{{ translate_string('general.localisedField.locationId.label', 'Location') }}",
-    to_char(encounter_start_datetime, '{{ var("date_format") }}') as "{{ translate_string('', 'Encounter start date') }}",
-    to_char(encounter_end_datetime, '{{ var("date_format") }}') as "{{ translate_string('', 'Encounter end date') }}",
-    to_char(encounter_start_datetime, '{{ var("time_format") }}') as "{{ translate_string('', 'Encounter start time') }}",
-    to_char(first_note_datetime, '{{ var("time_format") }}') as "{{ translate_string('', 'Notes start time') }}",
-    to_char(last_note_datetime, '{{ var("time_format") }}') as "{{ translate_string('', 'Notes end time') }}",
-    is_discharged as "{{ translate_string('', 'Discharges (has the patient been discharged)') }}",
-    non_discharge_by_clinicians as "{{ translate_string('', 'Non-discharge by clinicians') }}"
+    user_name as "{{ translate_string('userName', 'User name') }}",
+    user_role as "{{ translate_string('userRole', 'User role') }}",
+    display_id as "{{ translate_string('patientDisplayId','Patient ID') }}",
+    patient_category as "{{ translate_string('patientCategory', 'Patient category') }}",
+    triage_category as "{{ translate_string('triageCategory', 'Triage category') }}",
+    facility as "{{ translate_string('facilityName', 'Facility') }}",
+    department as "{{ translate_string('departmentName', 'Department') }}",
+    location_group as "{{ translate_string('areaName', 'Area') }}",
+    location as "{{ translate_string('locationName', 'Location') }}",
+    to_char(encounter_start_datetime, '{{ var("date_format") }}') as "{{ translate_string('encounterStartDate', 'Encounter start date') }}",
+    to_char(encounter_end_datetime, '{{ var("date_format") }}') as "{{ translate_string('encounterEndDate', 'Encounter end date') }}",
+    to_char(encounter_start_datetime, '{{ var("time_format") }}') as "{{ translate_string('encounterStartTime', 'Encounter start time') }}",
+    to_char(first_note_datetime, '{{ var("time_format") }}') as "{{ translate_string('notesStartTime', 'Notes start time') }}",
+    to_char(last_note_datetime, '{{ var("time_format") }}') as "{{ translate_string('notesEndTime', 'Notes end time') }}",
+    is_discharged as "{{ translate_string('isDischarged', 'Discharges (has the patient been discharged)') }}",
+    non_discharge_by_clinicians as "{{ translate_string('nonDischargeByClinician', 'Non-discharge by clinicians') }}"
 from {{ ref('ds__user_audit') }}
-where case
+where
+    case
         when {{ parameter('departmentId') }} is null then true
-        else department_id ={{ parameter('departmentId') }}
+        else department_id = {{ parameter('departmentId') }}
     end
     and
     case
         when {{ parameter('locationGroupId') }} is null then true
-        else location_group_id ={{ parameter('locationGroupId') }}
+        else location_group_id = {{ parameter('locationGroupId') }}
     end
