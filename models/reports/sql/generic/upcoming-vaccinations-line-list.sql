fe989f6ce479ba5a89be5174b04a445ccf9f8239
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-04-15 18:34:21 +1000

    Update upcoming-vaccinations-line-list.sql

--- models/reports/sql/generic/upcoming-vaccinations-line-list.sql
+++ models/reports/sql/generic/upcoming-vaccinations-line-list.sql
@@ -5,10 +5,10 @@ select
     to_char(date_of_birth, '{{ var("date_format") }}') as "{{ translate_string('patientDateOfBirth', 'Date of birth') }}",
     age as "{{ translate_string('patientAge', 'Age') }}",
     sex as "{{ translate_string('patientSex', 'Sex') }}",
-    to_char(due_date, '{{ var("date_format") }}') as "{{ translate_string('vaccineDueDate', 'Vaccination due date') }}",
+    to_char(due_date, '{{ var("date_format") }}') as "{{ translate_string('vaccinationDueDate', 'Vaccination due date') }}",
     vaccine_name as "{{ translate_string('vaccineName', 'Vaccine name') }}",
     vaccine_schedule as "{{ translate_string('vaccineSchedule', 'Schedule') }}",
-    vaccine_status as "{{ translate_string('vaccineStatus', 'Vaccine status') }}"
+    vaccine_status as "{{ translate_string('vaccinationStatus', 'Vaccine status') }}"
 from {{ ref("ds__patient_vaccinations_upcoming") }}
 where
     case
