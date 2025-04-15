b3f0c1cb1c8e408a551a8332542279e21d711cac
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-04-15 17:47:36 +1000

    Update upcoming-vaccinations-line-list.sql

--- models/reports/sql/generic/upcoming-vaccinations-line-list.sql
+++ models/reports/sql/generic/upcoming-vaccinations-line-list.sql
@@ -1,39 +1,37 @@
 select
-    display_id as "{{ translate_string('general.localisedField.displayId.label', 'Patient ID') }}",
-    first_name as "{{ translate_string('general.localisedField.firstName.label', 'First name') }}",
-    last_name as "{{ translate_string('general.localisedField.lastName.label', 'Last name') }}",
-    to_char(date_of_birth, '{{ var("date_format") }}') as "{{ translate_string('general.localisedField.dateOfBirth.label', 'Date of birth') }}",
-    age as "{{ translate_string('general.localisedField.Age', 'Age') }}",
-    sex as "{{ translate_string('general.localisedField.sex.label', 'Sex') }}",
-    to_char(due_date, '{{ var("date_format") }}') as "{{ translate_string('', 'Vaccination due date') }}",
-    vaccine_name as "{{ translate_string('vaccine.vaccineName.label', 'Vaccine name') }}",
-    vaccine_schedule as "{{ translate_string('vaccine.schedule.label', 'Schedule') }}",
-    vaccine_status as "{{ translate_string('general.localisedField.vaccinationStatus.label', 'Vaccine status') }}"
+    display_id as "{{ translate_string('patientDisplayId', 'Patient ID') }}",
+    first_name as "{{ translate_string('patientFirstName', 'First name') }}",
+    last_name as "{{ translate_string('patientLastName', 'Last name') }}",
+    to_char(date_of_birth, '{{ var("date_format") }}') as "{{ translate_string('patientDateOfBirth', 'Date of birth') }}",
+    age as "{{ translate_string('patientAge', 'Age') }}",
+    sex as "{{ translate_string('patientSex', 'Sex') }}",
+    to_char(due_date, '{{ var("date_format") }}') as "{{ translate_string('vaccineDueDate', 'Vaccination due date') }}",
+    vaccine_name as "{{ translate_string('vaccineName', 'Vaccine name') }}",
+    vaccine_schedule as "{{ translate_string('vaccineSchedule', 'Schedule') }}",
+    vaccine_status as "{{ translate_string('vaccineStatus', 'Vaccine status') }}"
 from {{ ref("ds__patient_vaccinations_upcoming") }}
 where
     case
         when {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }} is null then true
-        else date_of_birth::date
-            >={{ parameter('fromDate', default_value='2024-01-01', data_type='date') }}
+        else date_of_birth::date >= {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }}
     end
     and
     case
         when {{ parameter('toDate', default_value='2024-01-31', data_type='date') }} is null then true
-        else date_of_birth::date
-            <= {{ parameter('toDate', default_value='2024-01-31', data_type='date') }}
+        else date_of_birth::date <= {{ parameter('toDate', default_value='2024-01-31', data_type='date') }}
     end
     and
     case
         when {{ parameter('status') }} is null then true
-        else vaccine_status ={{ parameter('status') }}
+        else vaccine_status = {{ parameter('status') }}
     end
     and
     case
         when {{ parameter('category') }} is null then true
-        else vaccine_category ={{ parameter('category') }}
+        else vaccine_category = {{ parameter('category') }}
     end
     and
     case
         when {{ parameter('vaccine') }} is null then true
-        else vaccine_name ={{ parameter('vaccine') }}
+        else vaccine_name = {{ parameter('vaccine') }}
     end
