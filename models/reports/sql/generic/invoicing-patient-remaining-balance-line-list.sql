9b7ff145b4f9378858777a34cb155ec4b4551d4b
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-04-15 19:27:40 +1000

    Update invoicing-patient-remaining-balance-line-list.sql

--- models/reports/sql/generic/invoicing-patient-remaining-balance-line-list.sql
+++ models/reports/sql/generic/invoicing-patient-remaining-balance-line-list.sql
@@ -1,11 +1,11 @@
 select
-    to_char(i.discharge_datetime, '{{ var("date_format") }}') as "{{ translate_string('discharge.dischargeDate.label','Discharged date') }}",
-    i.invoice_number as "{{ translate_string('general.localisedField.invoiceDisplayId.label','Invoice number') }}",
-    i.patient_name as "{{ translate_string('general.table.column.patientName','Patient name') }}",
-    i.display_id as "{{ translate_string('general.localisedField.displayId.label','Patient ID') }}",
-    i.nationality as "{{ translate_string('general.localisedField.nationalityId.label','Nationality') }}",
-    i.total_patient_amount as "{{ translate_string('','Total patient amount') }}",
-    i.remaining_patient_balance as "{{ translate_string('','Remaining balance (patient)') }}"
+    to_char(i.discharge_datetime, '{{ var("date_format") }}') as "{{ translate_string('dischargeDate','Discharged date') }}",
+    i.invoice_number as "{{ translate_string('invoiceNumber','Invoice number') }}",
+    i.patient_name as "{{ translate_string('patientFullName','Patient name') }}",
+    i.display_id as "{{ translate_string('patientDisplayId','Patient ID') }}",
+    i.nationality as "{{ translate_string('patientNationality','Nationality') }}",
+    i.total_patient_amount as "{{ translate_string('invoicePatientAmount','Total patient amount') }}",
+    i.remaining_patient_balance as "{{ translate_string('invoiceRemainingPatientBalance','Remaining balance (patient)') }}"
 from {{ ref("ds__invoicing") }} i
 where i.status = 'finalised'
     and case
