a471d06d182c77d1dc5e1e499a576ff2ee82e9e0
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-04-15 17:55:10 +1000

    translation

--- models/reports/sql/generic/hospital-admissions-by-department-summary.sql
+++ models/reports/sql/generic/hospital-admissions-by-department-summary.sql
@@ -8,15 +8,15 @@ with reporting_months as (
 )
 
 select
-    to_char(rm.month, '{{ var("monthyear_format") }}') as "{{ translate_string('', 'Month') }}",
-    adh.facility as "{{ translate_string('general.localisedField.facility.label', 'Facility') }}",
-    adh.department as "{{ translate_string('general.localisedField.departmentId.label', 'Department') }}",
-    count(*) filter (where adh.admission) as "{{ translate_string('', 'Number of admissions') }}",
-    count(*) filter (where adh.discharge) as "{{ translate_string('', 'Number of discharges') }}",
-    count(*) filter (where adh.death) as "{{ translate_string('', 'Number of deaths') }}",
-    count(*) filter (where adh.transfer_in) as "{{ translate_string('', 'Number of transfers into department') }}",
-    count(*) filter (where adh.transfer_out) as "{{ translate_string('', 'Number of transfers out of department') }}",
-    round(avg(adh.length_of_stay), 1) as "{{ translate_string('', 'Average length of stay') }}"
+    to_char(rm.month, '{{ var("monthyear_format") }}') as "{{ translate_string('month', 'Month') }}",
+    adh.facility as "{{ translate_string('facilityName', 'Facility') }}",
+    adh.department as "{{ translate_string('departmentName', 'Department') }}",
+    count(*) filter (where adh.admission) as "{{ translate_string('admissionCount', 'Number of admissions') }}",
+    count(*) filter (where adh.discharge) as "{{ translate_string('dischargeCount', 'Number of discharges') }}",
+    count(*) filter (where adh.death) as "{{ translate_string('deathCount', 'Number of deaths') }}",
+    count(*) filter (where adh.transfer_in) as "{{ translate_string('transfersIntoDepartmentCount', 'Number of transfers into department') }}",
+    count(*) filter (where adh.transfer_out) as "{{ translate_string('transfersOutOfDepartmentCount', 'Number of transfers out of department') }}",
+    round(avg(adh.length_of_stay), 1) as "{{ translate_string('averageLengthOfStay', 'Average length of stay') }}"
 from reporting_months rm
 left join {{ ref('int__admission_history_department') }} adh
     on adh.start_datetime::date between rm.month and (rm.month + '1 month'::interval - '1 day'::interval)
@@ -32,3 +32,4 @@ group by
     adh.department_id,
     adh.department
 order by rm.month, adh.facility, adh.department
+
