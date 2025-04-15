93dbe6e79c4d66f92e05f78c02ceb30c5e3e0bc1
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-04-15 19:12:08 +1000

    translation

--- models/reports/sql/generic/invoicing-patient-payment-by-discharge-location-summary.sql
+++ models/reports/sql/generic/invoicing-patient-payment-by-discharge-location-summary.sql
@@ -1,12 +1,12 @@
 select
-    to_char(i.discharge_datetime, '{{ var("date_format") }}') as "{{ translate_string('discharge.dischargeDate.label','Discharged date') }}",
-    i.invoice_number as "{{ translate_string('general.localisedField.invoiceDisplayId.label','Invoice number') }}",
-    i.patient_name as "{{ translate_string('general.table.column.patientName','Patient name') }}",
-    i.discharge_area as "{{ translate_string('','Area (at time of discharge)') }}",
-    i.total_invoice_amount as "{{ translate_string('','Total invoice amount') }}",
-    i.total_insurer_amount as "{{ translate_string('','Total insurer amount') }}",
-    i.total_patient_discount as "{{ translate_string('','Total patient discount') }}",
-    i.total_patient_amount as "{{ translate_string('','Total patient amount') }}"
+    to_char(i.discharge_datetime, '{{ var("date_format") }}') as "{{ translate_string('dischargeDate','Discharged date') }}",
+    i.invoice_number as "{{ translate_string('invoiceNumber','Invoice number') }}",
+    i.patient_name as "{{ translate_string('patientName','Patient name') }}",
+    i.discharge_area as "{{ translate_string('dischargeArea','Area (at time of discharge)') }}",
+    i.total_invoice_amount as "{{ translate_string('totalInvoiceAmount','Total invoice amount') }}",
+    i.total_insurer_amount as "{{ translate_string('totalInsurerAmount','Total insurer amount') }}",
+    i.total_patient_discount as "{{ translate_string('totalPatientDiscount','Total patient discount') }}",
+    i.total_patient_amount as "{{ translate_string('totalPatientAmount','Total patient amount') }}"
 from {{ ref("ds__invoicing") }} i
 where i.status = 'finalised'
     and case
