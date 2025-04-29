e3be6db11aa8bc7cacc6f2027d7192e8e559e2d6
julianam-w <77252335+julianam-w@users.noreply.github.com>
2025-04-29 16:48:56 +1000

    Update

--- scripts/build_reporting_assets.py
+++ scripts/build_reporting_assets.py
@@ -21,7 +21,7 @@ def main():
     execute_command("dbt clean")
     execute_command("dbt deps")
     execute_command(f"dbt run --target {target} --profiles-dir config --select tag:{target}")
-    execute_command(f"dbt docs generate --target --profiles-dir config {target} --select tag:{target}")
+    execute_command(f"dbt docs generate --target {target} --profiles-dir config --select tag:{target}")
     execute_command(f"dbt compile --target {target} --profiles-dir config --select tag:{target}")
 
     # Hide macros and tests from documentation
