403e10c3b2efab2aeb293298bbb0897183d5c5d2
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-05-05 10:39:34 +1000

    MAUI-5244: On-going Conditions Line List (#43)

---
+++ models/reports/sql/generic/ongoing-conditions-line-list.sql
@@ -0,0 +1,25 @@
+select
+    display_id as "{{ translate_string('patientDisplayId', 'Patient ID') }}",
+    first_name as "{{ translate_string('patientFirstName', 'First name') }}",
+    last_name as "{{ translate_string('patientLastName', 'Last name') }}",
+    to_char(date_of_birth, '{{ var("date_format") }}') as "{{ translate_string('patientDateOfBirth', 'Date of birth') }}",
+    age as "{{ translate_string('patientAge', 'Age') }}",
+    sex as "{{ translate_string('patientSex', 'Sex') }}",
+    village as "{{ translate_string('patientVillage', 'Village') }}",
+    condition as "{{ translate_string('conditionOngoing', 'Ongoing condition') }}",
+    recorded_datetime as "{{ translate_string('conditionRecordedDate', 'Date recorded') }}",
+    clinician as "{{ translate_string('conditionRecordedBy', 'Clinician') }}",
+    date_resolved as "{{ translate_string('conditionResolvedDate', 'Date resolved') }}",
+    clinician_resolving as "{{ translate_string('conditionResolvedBy', 'Clinician confirming resolution') }}"
+from {{ ref('ds__ongoing_conditions') }}
+where case
+        when {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }} is null then true
+        else recorded_datetime::date
+            >= {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }}
+    end
+    and
+    case
+        when {{ parameter('toDate', default_value='2024-01-31', data_type='date') }} is null then true
+        else recorded_datetime::date
+            <= {{ parameter('toDate', default_value='2024-01-31', data_type='date') }}
+    end
