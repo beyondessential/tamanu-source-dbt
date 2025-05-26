34dad473bd7a56755920d794f8a6f22e94849a04
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-05-26 15:52:42 +1000

    Update invoicing-pending-line-list.sql

--- models/reports/sql/generic/invoicing-pending-line-list.sql
+++ models/reports/sql/generic/invoicing-pending-line-list.sql
@@ -3,9 +3,15 @@ select
     ip.patient_name as "{{ translate_label('patientName','Patient name') }}",
     ip.display_id as "{{ translate_label('patientDisplayId', 'Patient ID') }}"
 from {{ ref('ds__invoicing_pending') }} ip
-where i.id is null
-    and e.deleted_at is null
-    and e.end_date is not null
-    and p.id != 'h1627394-3778-4c31-a510-9fcb88efdbf3'
-    and case when :fromDate is null then true else e.end_date >= :fromDate end
-    and case when :toDate is null then true else e.end_date <= :toDate end
+where case
+        when {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }} is null then true
+        else ip.discharge_datetime::date
+            >= {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }}
+    end
+    and
+    case
+        when {{ parameter('toDate', default_value='2024-01-31', data_type='date') }} is null then true
+        else ip.discharge_datetime::date
+            <= {{ parameter('toDate', default_value='2024-01-31', data_type='date') }}
+    end
+
