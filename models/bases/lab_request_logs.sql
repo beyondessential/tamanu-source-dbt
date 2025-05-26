0fc4cd962510ebefb6c05c6ef98fac265a56055e
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-05-27 00:32:06 +1000

    report

---
+++ models/bases/lab_request_logs.sql
@@ -0,0 +1,13 @@
+select
+    lrl.id,
+    lrl.lab_request_id,
+    lrl.status,
+    lrl.updated_at,
+    lrl.updated_by_id
+from {{ source("tamanu", "lab_request_logs") }} lrl
+join {{ source("tamanu", "lab_requests") }} lr on lr.id = lrl.lab_request_id
+join {{ source("tamanu", "encounters") }} e on e.id = lr.encounter_id
+where lrl.deleted_at is null
+    and lr.deleted_at is null
+    and e.deleted_at is null
+    and e.patient_id != '{{ var("test_patient") }}'
