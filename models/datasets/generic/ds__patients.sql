4381dc2c7eedf488dbc3eaf8d97db6c43d933079
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-05-26 20:41:04 +1000

    update

--- models/datasets/generic/ds__patients.sql
+++ models/datasets/generic/ds__patients.sql
@@ -25,10 +25,10 @@ select
     occupation.name as occupation,
     religion.name as religion,
     billing.name as patient_billing_type,
-    case when pbd.id is null then 'Patient' else 'Birth' end as registration_type
+    case when pbd.patient_id is null then 'Patient' else 'Birth' end as registration_type
 from {{ ref("patients") }} p
 left join {{ ref("patient_additional_data") }} pad on pad.patient_id = p.id
-left join {{ ref("patient_birth_data") }} pbd on pbd.patient_id
+left join {{ ref("patient_birth_data") }} pbd on pbd.patient_id = p.id
 left join {{ ref("users") }} u on u.id = pad.registered_by_id
 join {{ ref("reference_data") }} village on village.id = p.village_id and village.type = 'village'
 left join {{ ref("reference_data") }} cob on cob.id = pad.country_of_birth_id and cob.type = 'country'
