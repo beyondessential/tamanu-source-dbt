2419207e70f5d949f8fd13d9e7859d5701658729
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-06-02 16:27:29 +1000

    update

--- models/bases/contributing_death_causes.sql
+++ models/bases/contributing_death_causes.sql
@@ -2,8 +2,7 @@ select
     cdc.id,
     cdc.time_after_onset,
     cdc.patient_death_data_id,
-    cdc.condition_id,
-    cdc.created_at
+    cdc.condition_id
 from {{ source("tamanu", "contributing_death_causes") }} cdc
 join {{ source("tamanu", "patient_death_data") }} pdd on pdd.id = cdc.patient_death_data_id
 where cdc.deleted_at is null
