ee53945ec266322414cfd46f6a5ed19590446446
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-06-02 16:20:38 +1000

    MAUI-5499: Lab Request Summary (#87)

--- models/bases/lab_requests.sql
+++ models/bases/lab_requests.sql
@@ -17,7 +17,8 @@ select
     lr.reason_for_cancellation,
     lr.published_date::timestamp as published_datetime,
     lr.encounter_id,
-    lr.department_id
+    lr.department_id,
+    lr.updated_at::timestamp as updated_datetime
 from {{ source("tamanu", "lab_requests") }} lr
 join {{ source("tamanu", "encounters") }} e on e.id = lr.encounter_id
 where lr.deleted_at is null
---
+++ models/reports/sql/generic/lab-requests-summary.sql
@@ -0,0 +1,55 @@
+with reporting_dates as (
+    select date::date as reporting_date
+    from generate_series(
+        {{ parameter('fromDate', default_value='2024-01-01', data_type='text') }}::date,
+        {{ parameter('toDate', default_value='2024-01-01', data_type='text') }}::date,
+        '1 day'::interval
+    ) date
+)
+
+select
+    rd.reporting_date as "{{ translate_label('reportingDate', 'Date') }}",
+    lrh.facility as "{{ translate_label('facilityName', 'Facility') }}",
+    lrh.department as "{{ translate_label('departmentName', 'Department') }}",
+    lrh.lab_test_category as "{{ translate_label('labTestCategory', 'Test category') }}",
+    count(distinct case
+        when rd.reporting_date = lrh.requested_date
+            then lrh.request_id
+    end) as "{{ translate_label('labRequestNewCount', 'Total new requests') }}",
+    count(case
+        when lrh.status = 'results_pending'
+            then 1
+    end) as "{{ translate_label('labRequestPendingCount', 'Total requests with a status of results pending') }}",
+    count(case
+        when lrh.status = 'published'
+            then 1
+    end) as "{{ translate_label('labRequestPublishedCount', 'Total requests published') }}"
+from reporting_dates rd
+join {{ ref('int__lab_requests_history') }} lrh
+    on lrh.status_start_date <= rd.reporting_date
+    and rd.reporting_date <= lrh.status_end_date
+where case
+        when {{ parameter('departmentId', default_value='null', data_type='text') }} is null
+            then true
+        else lrh.department_id::text = {{ parameter('departmentId', default_value='null', data_type='text') }}
+    end
+    and case
+        when {{ parameter('facilityId', default_value='null', data_type='text') }} is null
+            then true
+        else lrh.facility_id::text = {{ parameter('facilityId', default_value='null', data_type='text') }}
+    end
+    and case
+        when {{ parameter('labTestCategoryId', default_value='null', data_type='text') }} is null
+            then true
+        else lrh.lab_test_category_id::text = {{ parameter('labTestCategoryId', default_value='null', data_type='text') }}
+    end
+group by
+    rd.reporting_date,
+    lrh.facility,
+    lrh.department,
+    lrh.lab_test_category
+having count(case
+        when rd.reporting_date = lrh.requested_date
+            or lrh.status in ('results_pending', 'published')
+            then 1
+    end) > 0
