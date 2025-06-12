ca5c3bf069a5593f3213f17f1bd008158d4acb96
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-06-12 10:09:03 +1000

    update

--- models/reports/sql/msf/lab-follow-ups-line-list.sql
+++ models/reports/sql/msf/lab-follow-ups-line-list.sql
@@ -13,12 +13,12 @@ select
 from {{ ref('ds__lab_follow_ups') }}
 where case
         when {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }} is null then true
-        else requested_datetime::date >= {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }}
+        else follow_up_due_date::date >= {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }}
     end
     and
     case
         when {{ parameter('toDate', default_value='2024-01-31', data_type='date') }} is null then true
-        else requested_datetime::date <= {{ parameter('toDate', default_value='2024-01-31', data_type='date') }}
+        else follow_up_due_date::date <= {{ parameter('toDate', default_value='2024-01-31', data_type='date') }}
     end
     and case
         when {{ parameter('followUpStatus', default_value='null', data_type='text') }} is null
