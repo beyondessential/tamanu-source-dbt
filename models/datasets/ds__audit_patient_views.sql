fc2a7a8417ae5e4d5d35de73e9be549b87cd772f
julianam-w <77252335+julianam-w@users.noreply.github.com>
2025-07-12 16:23:06 +1000

    Update ds__audit_patient_views.sql

--- models/datasets/ds__audit_patient_views.sql
+++ models/datasets/ds__audit_patient_views.sql
@@ -22,4 +22,3 @@ join {{ ref('patients') }} p on p.id = lap.patient_id
 join {{ ref('users') }} u on u.id = lap.user_id
 left join {{ ref('facilities') }} f on f.id = lap.facility_id
 left join {{ ref('reference_data') }} village on village.id = p.village_id and village.type = 'village'
-order by lap.logged_at desc
