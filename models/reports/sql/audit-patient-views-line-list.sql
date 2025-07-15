3373c45c1476730252c36ce1efd1bceae03e7a9a
julianam-w <77252335+julianam-w@users.noreply.github.com>
2025-07-15 14:41:36 +1000

    MAUI-5589 Update audit patient views (#171)

--- models/reports/sql/audit-patient-views-line-list.sql
+++ models/reports/sql/audit-patient-views-line-list.sql
@@ -9,8 +9,7 @@ select
     viewed_by_user as "{{ translate_label('logAccessBy', 'Viewed by user') }}",
     user_email as "{{ translate_label('userEmail', 'User email') }}",
     user_role as "{{ translate_label('userRole', 'User role') }}",
-    viewed_at_facility as "{{ translate_label('logAccessAtFacility', 'Viewed at facility') }}",
-    to_char(date_time_viewed, '{{ var("datetime_format") }}') as "{{ translate_label('logAccessDatetime', 'Date and time viewed') }}"
+    to_char(date_time_viewed, '{{ var("datetime_without_seconds_format") }}') as "{{ translate_label('logAccessDatetime', 'Date and time viewed') }}"
 from {{ ref('ds__patient_access_logs') }}
 where
     case
@@ -32,4 +31,4 @@ where
         when {{ parameter('userId') }} is null then true
         else viewed_by_user_id = {{ parameter('userId') }}
     end
-order by date_time_viewed desc
+order by date_time_viewed
