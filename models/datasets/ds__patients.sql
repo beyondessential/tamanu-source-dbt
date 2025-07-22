114baa126b656832675db22aeba9beb4ca308842
gagank16 <gagank.1611@gmail.com>
2025-07-22 17:27:14 +1000

    Update

--- models/datasets/ds__patients.sql
+++ models/datasets/ds__patients.sql
@@ -25,7 +25,18 @@ select
     occupation.name as occupation,
     religion.name as religion,
     billing.name as patient_billing_type,
-    case when pbd.patient_id is null then 'Patient' else 'Birth' end as registration_type
+    case
+        when pbd.patient_id is null then 'Patient'
+        else 'Birth'
+    end as registration_type,
+    date_part(
+        'year',
+        age(p.date_of_birth)
+    ) as age,
+    case
+        when p.date_of_death is not null then 'Deceased'
+        else 'Alive'
+    end as status
 from {{ ref("patients") }} p
 left join {{ ref("patient_additional_data") }} pad on pad.patient_id = p.id
 left join {{ ref("patient_birth_data") }} pbd on pbd.patient_id = p.id
