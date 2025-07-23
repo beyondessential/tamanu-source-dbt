f76cef19618a1c250010f3fd688de75d62721ebc
julianam-w <77252335+julianam-w@users.noreply.github.com>
2025-07-23 18:28:05 +1000

    Update to v2.36 (#175)

--- models/reports/sql/invoicing-pending-line-list.sql
+++ models/reports/sql/invoicing-pending-line-list.sql
@@ -2,7 +2,7 @@ select
     to_char(discharge_datetime, '{{ var("datetime_format") }}') as "{{ translate_label('dischargeDate','Discharged date') }}",
     patient_name as "{{ translate_label('patientName','Patient name') }}",
     display_id as "{{ translate_label('patientDisplayId', 'Patient ID') }}"
-from {{ ref('ds__invoicing_pending') }} 
+from {{ ref('ds__invoicing_pending') }}
 where case
         when {{ parameter('fromDate', default_value='2024-01-01', data_type='date') }} is null then true
         else discharge_datetime::date
