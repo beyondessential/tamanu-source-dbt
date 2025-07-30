eb5049340d1586e236ccc728d3ed05507ef6c8a2
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-07-30 16:19:48 +1000

    update

---
+++ models/bases/patient_allergies.sql
@@ -0,0 +1,8 @@
+select
+    id,
+    patient_id,
+    allergy_id,
+    recorded_date::date as recorded_date,
+    practitioner_id as recorded_by
+from {{ source("tamanu", "patient_allergies") }}
+where deleted_at is null
