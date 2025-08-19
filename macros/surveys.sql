2e79e34b077215445fb72b19a0f981cacdd5c353
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-08-19 15:27:15 +1000

    Survey doc generation (#213)

--- macros/surveys.sql
+++ macros/surveys.sql
@@ -1,5 +1,5 @@
 {% macro get_survey_columns(survey_id) %}
-    SELECT pde.id, pde.name, pde.code
+    SELECT pde.id, pde.name, replace(pde.code,'-','_') as code
     FROM {{ source('tamanu', 'survey_screen_components') }} ssc
     JOIN {{ source('tamanu', 'program_data_elements') }} pde
     ON ssc.data_element_id = pde.id
@@ -17,6 +17,7 @@
         sra.response_id, 
         e.patient_id,
         sr.start_datetime,
+        sr.end_datetime,
         sr.result_text
     {%- for id, code in zip(columns['id'], columns['code']) %},
             MAX(CASE WHEN sra.data_element_id = '{{ id }}' THEN NULLIF(sra.body,'') END) AS "{{ code }}"
@@ -27,7 +28,7 @@
     JOIN {{ ref('encounters')}} e
     ON e.id = sr.encounter_id
     WHERE sr.survey_id = '{{ survey_id }}'
-    GROUP BY sra.response_id, sr.encounter_id, sr.start_datetime, sr.result_text, e.patient_id
+    GROUP BY sra.response_id, sr.encounter_id, sr.start_datetime, sr.end_datetime, sr.result_text, e.patient_id
 {% endmacro %}
 
 {% macro get_surveys_list() %}
