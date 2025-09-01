c912d5ee798be8ce531a8b936ac0e0c1dd8a871a
julianam-w <77252335+julianam-w@users.noreply.github.com>
2025-09-01 16:49:50 +1000

    Update outpatient-appointments-line-list.sql

--- models/reports/sql/outpatient-appointments-line-list.sql
+++ models/reports/sql/outpatient-appointments-line-list.sql
@@ -37,7 +37,7 @@ where case
     end
     and
     case
-        when coalesce({{ parameter('appointmentStatus') }}) is null then true
+        when {{ parameter('appointmentStatus') }} is null then true
         else appointment_status in ({{ parameter('appointmentStatus') }})
     end
     and
