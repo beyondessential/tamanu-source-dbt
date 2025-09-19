ea90897fb8b6cddda8f7494194e2f228dad18876
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-09-20 00:09:55 +1000

    generic encounter_prescription dataset

---
+++ models/datasets/ds__encounter_prescriptions.sql
@@ -0,0 +1,15 @@
+select
+    ep.encounter_id,
+    ep.prescription_id,
+    e.patient_id,
+    l.facility_id,
+    ep.is_selected_for_discharge,
+    p.medication_id,
+    m.name as medication,
+    p.quantity
+from {{ ref("encounter_prescriptions") }} ep
+join {{ ref("encounters")}} e on e.id = ep.encounter_id
+join {{ ref("prescriptions")}} p on p.id = ep.prescription_id
+join {{ ref("locations")}} l on l.id = e.location_id
+join {{ ref("reference_data")}} m on m.id = p.medication_id
+join {{ ref("users")}} u on u.id = p.prescriber_id
