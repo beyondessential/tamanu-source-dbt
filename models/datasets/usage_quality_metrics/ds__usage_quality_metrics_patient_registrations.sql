70eb72518fe26110b2af9c7f5f42f7d6d8e24ad3
julianam-w <77252335+julianam-w@users.noreply.github.com>
2025-10-08 14:50:19 +1100

    MAUI 5749 patient metrics v8 (#244)

---
+++ models/datasets/usage_quality_metrics/ds__usage_quality_metrics_patient_registrations.sql
@@ -0,0 +1,18 @@
+with data as (
+    select
+        p.registration_date,
+        p.id as registration_patient_id,
+        pbd.patient_id as birth_patient_id,
+        p.date_of_birth,
+        age(p.registration_date, p.date_of_birth) < interval '6 months' as age_under_6m_at_registration
+    from {{ ref("patients") }} p
+    left join {{ ref("patient_birth_data") }} pbd
+        on pbd.patient_id = p.id
+)
+select
+    registration_date,
+    count(*) filter (where birth_patient_id is null) as total_patient_registrations,
+    count(*) filter (where birth_patient_id notnull) as total_birth_registrations,
+    count(*) filter (where birth_patient_id is null and age_under_6m_at_registration) as total_incorrect_registrations_for_patient_under_6mth
+from data
+group by registration_date
