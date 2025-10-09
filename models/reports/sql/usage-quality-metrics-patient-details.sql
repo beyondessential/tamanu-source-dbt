2bcc8603361686ba2ea29ac8883f68eadb04b5c5
julianam-w <77252335+julianam-w@users.noreply.github.com>
2025-10-09 12:32:59 +1100

    Add missing quote marks (#248) (#250)

--- models/reports/sql/usage-quality-metrics-patient-details.sql
+++ models/reports/sql/usage-quality-metrics-patient-details.sql
@@ -1,9 +1,9 @@
 select
-    total_patients as {{ translate_label('patientTotal') }},
-    total_patients_with_incomplete_name as {{ translate_label('patientTotalWithIncompleteName') }},
-    total_patients_with_missing_dob as {{ translate_label('patientTotalWithMissingDateOfBirth') }},
-    total_patients_with_invalid_dob as {{ translate_label('patientTotalWithInvalidDateOfBirth') }},
-    total_patients_with_missing_location as {{ translate_label('patientTotalWithMissingLocation') }},
-    total_patients_with_missing_contact as {{ translate_label('patientTotalWithMissingContact') }},
-    total_patients_merged as {{ translate_label('patientTotalMerged') }}
+    total_patients as "{{ translate_label('patientTotal') }}",
+    total_patients_with_incomplete_name as "{{ translate_label('patientTotalWithIncompleteName') }}",
+    total_patients_with_missing_dob as "{{ translate_label('patientTotalWithMissingDateOfBirth') }}",
+    total_patients_with_invalid_dob as "{{ translate_label('patientTotalWithInvalidDateOfBirth') }}",
+    total_patients_with_missing_location as "{{ translate_label('patientTotalWithMissingLocation') }}",
+    total_patients_with_missing_contact as "{{ translate_label('patientTotalWithMissingContact') }}",
+    total_patients_merged as "{{ translate_label('patientTotalMerged') }}"
 from {{ ref("ds__usage_quality_metrics_patient_details") }}
