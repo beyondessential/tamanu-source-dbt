f5475d2306a3a929f145e0037df434143ba7cf45
julianam-w <77252335+julianam-w@users.noreply.github.com>
2025-10-30 12:17:52 +1100

    MAUI-5999 Patch for logs.changes (#281)

--- models/bases/patient_program_registration_conditions_change_logs.sql
+++ models/bases/patient_program_registration_conditions_change_logs.sql
@@ -4,6 +4,14 @@ with filtered_changes as (
             version = 'unknown'
             or string_to_array(version, '.')::int [] >= string_to_array('2.33.0', '.')::int []
         )
+{% if dbt_utils.get_relations_by_pattern('logs', 'changes_backup') %}
+    union all
+    {{ base_history_from_log('patient_program_registration_conditions', 'logs__tamanu', 'changes_backup') }}
+        and (
+            version = 'unknown'
+            or string_to_array(version, '.')::int [] >= string_to_array('2.33.0', '.')::int []
+        )
+{% endif %}
 )
 
 select
--- models/bases/patients_change_logs.sql
+++ models/bases/patients_change_logs.sql
@@ -1,6 +1,25 @@
+{%- set columns = [
+    {'expr': "fc.record_data ->> 'display_id'", 'name': 'display_id', 'is_direct_identifier': true},
+    {'expr': "fc.record_data ->> 'first_name'", 'name': 'first_name', 'is_direct_identifier': true},
+    {'expr': "fc.record_data ->> 'middle_name'", 'name': 'middle_name', 'is_direct_identifier': true},
+    {'expr': "fc.record_data ->> 'last_name'", 'name': 'last_name', 'is_direct_identifier': true},
+    {'expr': "fc.record_data ->> 'cultural_name'", 'name': 'cultural_name', 'is_direct_identifier': true},
+    {'expr': "fc.record_data ->> 'email'", 'name': 'email', 'is_direct_identifier': true},
+    {'expr': "initcap(fc.record_data ->> 'sex')", 'name': 'sex', 'is_direct_identifier': false},
+    {'expr': "(fc.record_data ->> 'date_of_birth')::date", 'name': 'date_of_birth', 'is_direct_identifier': false},
+    {'expr': "(fc.record_data ->> 'date_of_death')::timestamp", 'name': 'date_of_death', 'is_direct_identifier': false},
+    {'expr': "fc.record_data ->> 'village_id'", 'name': 'village_id', 'is_direct_identifier': false},
+    {'expr': "(fc.record_data ->> 'created_at')::date", 'name': 'registration_date', 'is_direct_identifier': false}
+] -%}
+
 with filtered_changes as (
     {{ base_history_from_log('patients') }}
         and record_id != '{{ var("test_patient") }}'
+{% if dbt_utils.get_relations_by_pattern('logs', 'changes_backup') %}
+    union all
+    {{ base_history_from_log('patients', 'logs__tamanu', 'changes_backup') }}
+        and record_id != '{{ var("test_patient") }}'
+{% endif %}
 )
 
 select
@@ -8,19 +27,6 @@ select
     fc.logged_at at time zone '{{ var("timezone") }}' as logged_at,
     fc.updated_by_user_id,
     fc.record_id as id
-    {%- set columns = [
-        {'expr': "fc.record_data ->> 'display_id'", 'name': 'display_id', 'is_direct_identifier': true},
-        {'expr': "fc.record_data ->> 'first_name'", 'name': 'first_name', 'is_direct_identifier': true},
-        {'expr': "fc.record_data ->> 'middle_name'", 'name': 'middle_name', 'is_direct_identifier': true},
-        {'expr': "fc.record_data ->> 'last_name'", 'name': 'last_name', 'is_direct_identifier': true},
-        {'expr': "fc.record_data ->> 'cultural_name'", 'name': 'cultural_name', 'is_direct_identifier': true},
-        {'expr': "fc.record_data ->> 'email'", 'name': 'email', 'is_direct_identifier': true},
-        {'expr': "initcap(fc.record_data ->> 'sex')", 'name': 'sex', 'is_direct_identifier': false},
-        {'expr': "(fc.record_data ->> 'date_of_birth')::date", 'name': 'date_of_birth', 'is_direct_identifier': false},
-        {'expr': "(fc.record_data ->> 'date_of_death')::timestamp", 'name': 'date_of_death', 'is_direct_identifier': false},
-        {'expr': "fc.record_data ->> 'village_id'", 'name': 'village_id', 'is_direct_identifier': false},
-        {'expr': "(fc.record_data ->> 'created_at')::date", 'name': 'registration_date', 'is_direct_identifier': false}
-    ] -%}
     {%- for col in columns -%}
         {%- if not (is_analytics_target() and col.is_direct_identifier) -%}
             ,
