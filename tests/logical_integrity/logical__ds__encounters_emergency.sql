2c4ee95ab1546cb331f7277c4c691d1fba836d6a
julianam-w <77252335+julianam-w@users.noreply.github.com>
2025-11-06 12:31:13 +1100

    Patient emergency encounter summary v2.40 (#292)

---
+++ tests/logical_integrity/logical__ds__encounters_emergency.sql
@@ -0,0 +1,12 @@
+with base as (
+    select count(*) as total_encounters
+    from {{ ref('triages') }} t
+),
+dataset as (
+    select count(*) as total_encounters
+    from {{ ref('ds__encounters_emergency') }}
+)
+select base.total_encounters as base_total_encounters,
+       dataset.total_encounters as dataset_total_encounters
+from base
+join dataset on dataset.total_encounters != base.total_encounters
