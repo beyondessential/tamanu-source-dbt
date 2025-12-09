bf5dbdddd0c127aa072b44ab57eaddd4709e8cdf
julianam-w <77252335+julianam-w@users.noreply.github.com>
2025-12-09 19:51:33 +1100

    Upgrade to 2.46

--- models/bases/notes.sql
+++ models/bases/notes.sql
@@ -1,14 +1,17 @@
 -- May include notes for the test patient.
 select
-    id,
-    date::timestamp as datetime,
-    content,
-    note_type,
-    record_type,
-    record_id,
-    author_id as authored_by_id,
-    on_behalf_of_id,
-    revised_by_id as updated_note_id,
-    visibility_status
-from {{ resolve_input_model('notes') }}
-where deleted_at is null
+    n.id,
+    n.date::timestamp as datetime,
+    n.content,
+    n.note_type_id,
+    rd.code as note_type,
+    n.record_type,
+    n.record_id,
+    n.author_id as authored_by_id,
+    n.on_behalf_of_id,
+    n.revised_by_id as updated_note_id,
+    n.visibility_status
+from {{ resolve_input_model('notes') }} n
+join {{ resolve_input_model('reference_data') }} rd
+    on rd.id = n.note_type_id
+where n.deleted_at is null
