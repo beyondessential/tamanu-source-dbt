c8ba04ede2fa6321f846e31aaf36516254c5f440
Salman <114740396+hrazasalman@users.noreply.github.com>
2025-12-23 14:41:31 +1100

    Update metadata.sql

--- macros/models/metadata.sql
+++ macros/models/metadata.sql
@@ -1,8 +1,8 @@
 {%- macro get_metadata_from_changes(table_name) -%}
 select 
     record_id as id,
-    min(logged_at) as created_datetime,
-    max(logged_at) as updated_datetime
+    min(record_created_at) as created_datetime,
+    max(record_updated_at) as updated_datetime
 from {{ source('logs__tamanu', 'changes') }}
 where table_name = '{{ table_name }}'
 group by record_id
