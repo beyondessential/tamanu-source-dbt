67ee6915a8e35335c5ad3e8e310afeb77865316d
Salman <114740396+hrazasalman@users.noreply.github.com>
2026-01-05 11:44:04 +1100

    Update metadata.sql

--- macros/models/metadata.sql
+++ macros/models/metadata.sql
@@ -3,8 +3,7 @@ with change_logs as (
     select 
         record_id,
         logged_at,
-        record_created_at, 
-        record_updated_at
+        least(record_created_at, logged_at) as created_datetime
     from {{ source('logs__tamanu', 'changes') }}
     where table_name = '{{ table_name }}'
     {% if dbt_utils.get_relations_by_pattern(
@@ -15,15 +14,14 @@ with change_logs as (
     select 
         record_id,
         logged_at,
-        record_created_at, 
-        record_updated_at
+        least(record_created_at, logged_at) as created_datetime
     from {{ source('logs__tamanu', 'changes_backup') }}
     where table_name = '{{ table_name }}'
     {% endif %}
 )
 select 
     record_id as id,
-    min(coalesce(record_created_at, logged_at)) as created_datetime,
+    min(created_datetime) as created_datetime,
     max(logged_at) as updated_datetime
 from change_logs
 group by record_id
