6a73d43862bda1cf91c980cf5ed180ecc705a770
julianam-w <77252335+julianam-w@users.noreply.github.com>
2026-01-05 23:32:51 +1100

    Bugfix on metadata macro v2.44 (#321)

--- macros/models/metadata.sql
+++ macros/models/metadata.sql
@@ -1,23 +1,27 @@
 {%- macro get_metadata_from_changes(table_name) -%}
-with combined_changes as (
+with change_logs as (
     select 
         record_id,
-        logged_at
+        logged_at,
+        least(record_created_at, logged_at) as created_datetime
     from {{ source('logs__tamanu', 'changes') }}
     where table_name = '{{ table_name }}'
-    {% if dbt_utils.get_relations_by_pattern('logs', 'changes_backup') %}
-        union all
-        select 
-            record_id,
-            logged_at
-        from {{ source('logs__tamanu', 'changes_backup') }}
-        where table_name = '{{ table_name }}'
+    {% if dbt_utils.get_relations_by_pattern(
+        schema_pattern='logs',
+        table_pattern='changes_backup') %}
+    union all
+    select 
+        record_id,
+        logged_at,
+        least(record_created_at, logged_at) as created_datetime
+    from {{ source('logs__tamanu', 'changes_backup') }}
+    where table_name = '{{ table_name }}'
     {% endif %}
 )
 select 
-    cc.record_id as id,
-    min(cc.logged_at) as created_datetime,
-    max(cc.logged_at) as updated_datetime
-from combined_changes cc
-group by cc.record_id
+    record_id as id,
+    min(created_datetime) as created_datetime,
+    max(logged_at) as updated_datetime
+from change_logs
+group by record_id
 {%- endmacro %}
