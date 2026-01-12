53b9352fd06c16e1ff335a790e818df33efaf06b
julianam-w <77252335+julianam-w@users.noreply.github.com>
2026-01-12 15:11:56 +1100

    Shift to use metadata from public models

--- models/bases/lab_request_logs.sql
+++ models/bases/lab_request_logs.sql
@@ -1,5 +1,7 @@
 select
     lrl.id,
+    lrl.created_at as created_datetime,
+    lrl.updated_at as updated_datetime,
     lrl.lab_request_id,
     lrl.status,
     lrl.updated_by_id
--- models/datasets/usage_quality_metrics/ds__usage_quality_metrics_patient_registrations.sql
+++ models/datasets/usage_quality_metrics/ds__usage_quality_metrics_patient_registrations.sql
@@ -1,12 +1,11 @@
 with data as (
     select
-        pm.created_datetime as registration_date,
+        p.created_datetime as registration_date,
         p.id as registration_patient_id,
         pbd.patient_id as birth_patient_id,
         p.date_of_birth,
-        age(pm.created_datetime, p.date_of_birth) < interval '6 months' as age_under_6m_at_registration
+        age(p.created_datetime, p.date_of_birth) < interval '6 months' as age_under_6m_at_registration
     from {{ ref("patients") }} p
-    join {{ ref("patients_metadata") }} pm on pm.id = p.id
     left join {{ ref("patient_birth_data") }} pbd
         on pbd.patient_id = p.id
 )
