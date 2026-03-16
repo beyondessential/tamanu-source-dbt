c32e6125fde06dc397c8dbec9035349282daa69a
julianam-w <77252335+julianam-w@users.noreply.github.com>
2026-03-16 13:44:21 +1100

    Add patient care plan and patient family history base models v2.45 (#377)

---
+++ models/bases/patient_family_histories.sql
@@ -0,0 +1,11 @@
+select
+    id,
+    recorded_date::timestamp as recorded_datetime,
+    patient_id,
+    practitioner_id as clinician_id,
+    diagnosis_id,
+    relationship,
+    note
+from {{ resolve_input_model('patient_family_histories') }}
+where deleted_at is null
+    and patient_id != '{{ var("test_patient") }}'
