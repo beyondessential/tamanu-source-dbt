59c9d7b951b9dd92b997fa95bac44996bb6a10a2
julianam-w <77252335+julianam-w@users.noreply.github.com>
2026-03-22 22:03:26 +1100

    Update

--- models/reports/sql/admin/admin-invoice-products-summary.sql
+++ models/reports/sql/admin/admin-invoice-products-summary.sql
@@ -25,7 +25,7 @@ select
     category as "{{ translate_label('invoiceProductCategory') }}",
     source_record_id as "{{ translate_label('invoiceProductCategoryId') }}",
     visibility_status as "{{ translate_label('invoiceProductVisibilityStatus') }}",
-    external_code as "Lab ex. code"
+    external_code as "{{ translate_label('invoiceProductLabExternalCode') }}"
     {%- for name in price_list_names %}
     , "Price: {{ name }}"
     {%- endfor %}
