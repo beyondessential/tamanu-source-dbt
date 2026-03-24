172d2f1427ea955e9b61ac26b9acf6f32e3bbd0a
gagank16 <111413086+gagank16@users.noreply.github.com>
2026-03-24 15:08:57 +1100

    feat(reports): add encounter summary by start/end date reports v2.49 (#397)

---
+++ models/bases/invoices.sql
@@ -0,0 +1,14 @@
+select
+    i.id,
+    i.display_id,
+    i.date::timestamp as datetime,
+    i.status,
+    i.patient_payment_status,
+    i.insurer_payment_status,
+    i.encounter_id
+from {{ resolve_input_model('invoices') }} i
+join {{ resolve_input_model('encounters') }} e on e.id = i.encounter_id
+where
+    i.deleted_at is null
+    and e.deleted_at is null
+    and e.patient_id != '{{ var("test_patient") }}'
---
+++ models/reports/sql/standard/encounter-summary-by-end-date.sql
@@ -0,0 +1,1 @@
+{{ encounter_summary_report('end_datetime', is_sensitive=false) }}
