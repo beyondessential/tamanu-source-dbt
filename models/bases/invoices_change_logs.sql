72c9b987495b7df83b6f38124fd5ad0e19d940c3
julianam-w <77252335+julianam-w@users.noreply.github.com>
2026-04-09 13:42:01 +1000

    fix(bases): add type cast to invoices_change_logs (#413)

--- models/bases/invoices_change_logs.sql
+++ models/bases/invoices_change_logs.sql
@@ -1,6 +1,6 @@
 select
     c.id as change_id,
-    c.record_id as invoice_id,
+    c.record_id::uuid as invoice_id,
     c.logged_at,
     c.updated_by_user_id,
     c.record_data ->> 'status' as status,
@@ -14,7 +14,7 @@ select
         order by c.logged_at, c.record_updated_at, c.id
     ) as change_sequence
 from {{ source('logs__tamanu', 'changes') }} c
-join {{ source('tamanu', 'invoices') }} i on i.id = c.record_id
+join {{ source('tamanu', 'invoices') }} i on i.id = c.record_id::uuid
     and i.deleted_at is null
 join {{ source('tamanu', 'encounters') }} e on e.id = i.encounter_id
     and e.deleted_at is null
