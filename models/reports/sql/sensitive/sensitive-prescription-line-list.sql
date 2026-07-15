a84f771c59b4d84a48f4041e843073f40561f52f
Salman <salman@bes.au>
2026-07-15 13:41:56 +1000

    feat: Upgrade Tamanu version to 2.60.0 (#547)
    - Updated dbt_project.yml to version 2.60.0
    - Refreshed Tamanu source tables from release/2.60
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- models/reports/sql/sensitive/sensitive-prescription-line-list.sql
+++ models/reports/sql/sensitive/sensitive-prescription-line-list.sql
@@ -18,7 +18,9 @@ select
     is_prn as "{{ translate_label('prescriptionIsPRN') }}",
     is_variable_dose as "{{ translate_label('prescriptionIsVariableDose') }}",
     dose_amount as "{{ translate_label('prescriptionDoseAmount') }}",
-    units as "{{ translate_label('prescriptionUnits') }}",
+    dosing_unit as "{{ translate_label('prescriptionDosingUnit') }}",
+    dispensing_unit as "{{ translate_label('prescriptionDispensingUnit') }}",
+    unit_conversion as "{{ translate_label('prescriptionUnitConversion') }}",
     frequency as "{{ translate_label('prescriptionFrequency') }}"
 from {{ ref('ds__sensitive_encounter_prescriptions') }}
 where
